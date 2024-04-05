d751459907c522c7d6981a6d0ffb65aec10e2c67
ethanrwilson1998 <ethanrwilson1998@gmail.com>
2024-04-05 10:49:39 -0400

    use DeepFace to tag utility attributes

--- src/evaluation/utility/utility_evaluation.py
+++ src/evaluation/utility/utility_evaluation.py
@@ -4,17 +4,28 @@ import os
 from pathlib import Path
 
 import cv2
-import insightface
-import numpy as np
 import pandas as pd
-from hsemotion.facial_emotions import HSEmotionRecognizer
+from deepface import DeepFace
 from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 
-from src import utils
-from src.config import EMOTION_MODEL
 from src.evaluation.evaluator import generate_key
 from src.privacy_mechanisms.privacy_mechanism import PrivacyMechanism
+
+
+def collect_utility_metrics(img_paths: list) -> dict:
+    outer_dict = dict()
+
+    for path in tqdm(img_paths, desc="DeepFace analysis"):
+        inner_dict = DeepFace.analyze(
+            img_path=path, actions=["age", "gender", "race", "emotion"]
+        )
+        img = cv2.imread(path)
+        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        inner_dict["greyscale"] = grey
+        outer_dict[path] = inner_dict
+
+    return outer_dict
 
 
 def utility_evaluation(
@@ -23,20 +34,7 @@ def utility_evaluation(
 ):
     print("================ Utility Evaluation ================")
 
-    detect_model, _ = utils.load_insightface_models()
-
-    # emotion recognition model is from: https://github.com/av-savchenko/face-emotion-recognition
-    # using their released python package
-    try:
-        emotion_model = HSEmotionRecognizer(model_name=EMOTION_MODEL, device="cuda")
-        print("Loaded HSEmotion model on GPU.")
-    except Exception as e:
-        print(f"Unable to load HSEmotion model on GPU, {e}.")
-        emotion_model = HSEmotionRecognizer(model_name=EMOTION_MODEL, device="cpu")
-        print("Loaded HSEmotion model on CPU.")
-
-    # need to compute a list of anonymized face images, then get the
-    # corresponding real faces
+    # need to compute a list of anonymized face images, then get the corresponding real faces
     if args.anonymized_dataset is None:
         anon_paths = glob.glob(
             f"Anonymized Datasets//{args.dataset}_{p_mech_object.get_suffix()}//**//*.jpg",
@@ -62,6 +60,9 @@ def utility_evaluation(
             )
         real_paths.append(r_p)
 
+    anon_dict = collect_utility_metrics(anon_paths)
+    real_dict = collect_utility_metrics(real_paths)
+
     out_data = []
 
     for r_p, a_p in tqdm(
@@ -69,70 +70,35 @@ def utility_evaluation(
         desc="Analyzing utility pair-wise...",
         total=len(anon_paths),
     ):
-        # prepare the faces
-        real_img = cv2.imread(r_p)
-        anon_img = cv2.imread(a_p)
-        bboxes, kpss = detect_model.detect(real_img)
+        this_anon_dict = anon_dict[a_p]
+        this_real_dict = real_dict[r_p]
 
-        if len(bboxes) > 0:
-            real_face = insightface.utils.face_align.norm_crop(
-                real_img, landmark=kpss[0]
-            )
-            anon_face = insightface.utils.face_align.norm_crop(
-                anon_img, landmark=kpss[0]
-            )
-            faces_detected = True
-        else:
-            real_face, anon_face = None, None
-            faces_detected = False
-
-        # structural similarity
-        ssim_img = ssim(
-            cv2.cvtColor(real_img, cv2.COLOR_BGR2GRAY),
-            cv2.cvtColor(anon_img, cv2.COLOR_BGR2GRAY),
-            data_range=255,
+        ssim_score = ssim(this_anon_dict["greyscale"], this_real_dict["greyscale"])
+        emotion_score = (
+            1 if this_anon_dict["emotion"] == this_real_dict["emotion"] else 0
         )
-        ssim_face = (
-            ssim(
-                cv2.cvtColor(real_face, cv2.COLOR_BGR2GRAY),
-                cv2.cvtColor(anon_face, cv2.COLOR_BGR2GRAY),
-                data_range=255,
-            )
-            if faces_detected
-            else None
-        )
-
-        # emotion recognition
-        emotion_class, emotion_prob_err = None, None
-        # list of emotions is: Anger, Contempt, Disgust, Fear, Happiness, Neutral, Sadness, Surprise
-        if faces_detected:
-            real_emotion, real_scores = emotion_model.predict_emotions(
-                real_face, logits=False
-            )
-            anon_emotion, anon_scores = emotion_model.predict_emotions(
-                anon_face, logits=False
-            )
-
-            emotion_class = 1 if real_emotion == anon_emotion else 0
-            emotion_prob_err = np.abs(
-                np.max(real_scores) - anon_scores[np.argmax(real_scores)]
-            )
+        age_score = 1 if this_anon_dict["age"] == this_real_dict["age"] else 0
+        race_score = 1 if this_anon_dict["race"] == this_real_dict["race"] else 0
+        gender_score = 1 if this_anon_dict["gender"] == this_real_dict["gender"] else 0
 
         out_data.append(
             {
                 "key": generate_key(r_p),
-                "ssim_img": ssim_img,
-                "ssim_face": ssim_face,
-                "emotion_class": emotion_class,
-                "emotion_prob_err": emotion_prob_err,
+                "ssim": ssim_score,
+                "emotion": emotion_score,
+                "age": age_score,
+                "race": race_score,
+                "gender": gender_score,
             }
         )
 
     df = pd.DataFrame(out_data)
     print("================ Utility Results ================")
-    print(f"SSIM: {df['ssim_img'].mean():.4f}")
-    print(f"Emotion classification: {df['emotion_class'].mean():.4f}")
-    print(f"Emotion probability error: {df['emotion_prob_err'].mean():.4f}")
+    print(f"SSIM: {df['ssim'].mean():.4f}")
+    print(f"Emotion Acc: {df['emotion'].mean():.4f}")
+    print(f"Age Acc: {df['age'].mean():.4f}")
+    print(f"Race Acc: {df['race'].mean():.4f}")
+    print(f"Gender Acc: {df['gender'].mean():.4f}")
 
     if args.anonymized_dataset is None:
         out_path = f"Results//Utility//{args.evaluation_method}//{args.dataset}_{p_mech_object.get_suffix()}.csv"
