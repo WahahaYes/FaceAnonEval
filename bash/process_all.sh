179e57f7709bfc49858ed5023b370376471864d0
Ethan Wilson <ethanrwilson1998@gmail.com>
2025-07-23 09:56:52 -0400

    removed test/eval files not related to public codebase

--- bash/process_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sh ./bash/process_blur.sh
-sh ./bash/process_dtheta_privacy.sh
-sh ./bash/process_identity_dp.sh
-sh ./bash/process_metric_privacy.sh
-sh ./bash/process_pixel_dp.sh
-sh ./bash/process_simswap.sh
--- lfw_test.sh
+++
@@ -1,3 +0,0 @@
-python process_dataset.py --dataset CelebA --privacy_mechanism $1 --faceswap_strategy ssim_similarity --ssim_sample_size 1
-
-$SHELL
